b5705f0107de18c52f065c291bb29d3842493516
Mag Kafel <na.hasiok@gmail.com>
2019-02-19 11:16:39 +0000

    Creating db and db queries

---
+++ db_connection.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+
+def create_table(conn):
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS task_management(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, status TEXT, important TEXT, date TEXT)")
+    conn.commit()
+    c.close()
+    
+
+def add_task(conn, name, description, status, important, date):
+    c = conn.cursor()
+    c.execute('''INSERT INTO task_management(name, description, status, important, date) 
+        VALUES(?, ?, ?, ?, ?)''',
+        (name, description, status, important, date))
+    conn.commit()
+    c.close()
+    
+
+def delete_task(conn, id):
+    c = conn.cursor()
+    c.execute(('DELETE FROM task_management WHERE name = ?'), (id,))
+    conn.commit()
+    c.close()
+    
+    
+def update_task(conn, id, name, description, status, important, date):
+    c = conn.cursor()
+    c.execute('''UPDATE task_management
+               SET name=?, description=?, status=?, important=?, date=?
+               WHERE id=?''',
+               (name, description, status, important, date, id))
+    conn.commit()
+    c.close()
+    
+        
+def get_all_tasks(conn):
+    c = conn.cursor()
+    c.execute('SELECT id, name, description, status, important, date FROM task_management')
+    columns = ["id", "name", "description", "status", "important", "date"]
+    result = [dict(zip(columns, row)) for row in c.fetchall()] #combines two lists into a list of tuples
+    print(result)
+    c.close()
+    return result
+    
+
+# get_tasks_for_date(conn, date)
+#def get_tasks(conn, date):
+#    c = conn.cursor()
+#    c.execute('SELECT date FROM task_management')
+#    columns = ["id", "name", "description", "status", "important", "date"]
+#    result = [dict(zip(columns, row)) for row in c.fetchall()] #combines two lists into a list of tuples
+#    print(result)
+#    c.close()
+#    return result
+
+# get_tasks_between_dates(conn, from_date, to_date)
+#def get_tasks_between_date(conn, from_date, to_date):
+#    c = conn.cursor()
+#    c.execute('SELECT * FROM task_management WHERE date between 'yyyy-mm-dd' and 'yyyy-mm-dd')
+#    columns = ["id", "name", "description", "status", "important", "date"]
+#    result = [dict(zip(columns, row)) for row in c.fetchall()] #combines two lists into a list of tuples
+#    print(result)
+#    c.close()
+#    return result
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect("task_management.db")
+    create_table(conn)
+    conn.close()
